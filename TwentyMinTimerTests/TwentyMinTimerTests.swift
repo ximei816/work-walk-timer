69f5402e73eded27835e0cc88617fe0572146f05
Nozomi Mitsuo <ximei816@gmail.com>
2020-04-06 15:02:20 +0800

    Initial Commit

---
+++ TwentyMinTimerTests/TwentyMinTimerTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwentyMinTimerTests.swift
+//  TwentyMinTimerTests
+//
+//  Created by 満尾希美 on 6/4/20.
+//  Copyright © 2020 NM. All rights reserved.
+//
+
+import XCTest
+@testable import TwentyMinTimer
+
+class TwentyMinTimerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
